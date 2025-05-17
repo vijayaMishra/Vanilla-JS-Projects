77a02d20a35266a770939d9dd7912a5bc6acce33
Vijaya <vijayamishra1709@gmail.comgit>
2025-05-17 16:37:31 +0530

    doing mistake of calling the function before clicking button

--- BoxUI/script.js
+++ BoxUI/script.js
@@ -1,45 +1,29 @@
 const leftButtonElement = document.querySelector(".leftButton");
 let outerBoxElement = document.querySelector(".outer-box");
-leftButtonElement.addEventListener("click", function() {
-    //console.log(" left button: ", leftButton);
-    //console.log(" element class list", leftButton.classList);
-    outerBoxElement.classList.remove("moveToTopLeft");
-    outerBoxElement.classList.remove("moveToTopRight");
-    outerBoxElement.classList.remove("moveToBottomRight");
-    outerBoxElement.classList.remove("moveToTopCenter");
-    outerBoxElement.classList.remove("moveToBottomCenter");
-    outerBoxElement.classList.add("moveToLeft");
+
+function movePosition(classPosition) {
+    outerBoxElement.classList.remove("moveToLeft", "moveToTopLeft", "moveToTopRight", "moveToBottomRight", "moveToTopCenter", "moveToBottomCenter");
+    outerBoxElement.classList.add(classPosition);
+    
+}
+
+leftButtonElement.addEventListener("click", function () {
+    movePosition("moveToLeft")
 });
 
 const topLeftButtonElement = document.querySelector(".topLeftButton");
-topLeftButtonElement.addEventListener("click", function() {
-    console.log("inside top left button");
-    outerBoxElement.classList.remove("moveToLeft");
-    outerBoxElement.classList.remove("moveToTopRight");
-    outerBoxElement.classList.remove("moveToBottomRight");
-    outerBoxElement.classList.remove("moveToTopCenter");
-    outerBoxElement.classList.remove("moveToBottomCenter");
-    outerBoxElement.classList.add("moveToTopLeft");
-});
+topLeftButtonElement.addEventListener("click", movePosition("moveToTopLeft"));
 
 const topRightButtonElement = document.querySelector(".topRightButton");
 topRightButtonElement.addEventListener("click", function() {
-    outerBoxElement.classList.remove("moveToLeft");
-    outerBoxElement.classList.remove("moveToTopLeft");
-    outerBoxElement.classList.remove("moveToBottomRight");
-    outerBoxElement.classList.remove("moveToTopCenter");
-    outerBoxElement.classList.remove("moveToBottomCenter");
-    outerBoxElement.classList.add("moveToTopRight");
+    outerBoxElement.classList.remove("moveToLeft", "moveToTopLeft", "moveToTopRight", "moveToBottomRight", "moveToTopCenter", "moveToBottomCenter");
+    outerBoxElement.classList.add("moveToTopRight");    
 });
 
 const bottomRightElement = document.querySelector(".bottomRightButton");
 bottomRightElement.addEventListener("click", function() {
-    outerBoxElement.classList.remove("moveToLeft");
-    outerBoxElement.classList.remove("moveToTopLeft");
-    outerBoxElement.classList.remove("moveToTopRight");
-    outerBoxElement.classList.remove("moveToTopCenter");
-    outerBoxElement.classList.remove("moveToBottomCenter");
-    outerBoxElement.classList.add("moveToBottomRight");
+    outerBoxElement.classList.remove("moveToLeft", "moveToTopLeft", "moveToTopRight", "moveToBottomRight", "moveToTopCenter", "moveToBottomCenter");
+    outerBoxElement.classList.add("moveToBottomRight");   
 });
 
 const topCenterElement = document.querySelector(".topCenterButton");
@@ -61,3 +45,6 @@ bottomCenterElement.addEventListener("click", function() {
     outerBoxElement.classList.remove("moveToTopCenter");
     outerBoxElement.classList.add("moveToBottomCenter");
 });
+
+
+
