0398b59cb627fdafb3c1eac7eb03273b262620b8
Vijaya <vijayamishra1709@gmail.comgit>
2025-05-18 17:27:11 +0530

    Code refactoring to avoid repetitive statements for adding and removing class in element.

--- BoxUI/script.js
+++ BoxUI/script.js
@@ -1,50 +1,37 @@
-const leftButtonElement = document.querySelector(".leftButton");
 let outerBoxElement = document.querySelector(".outer-box");
 
 function movePosition(classPosition) {
     outerBoxElement.classList.remove("moveToLeft", "moveToTopLeft", "moveToTopRight", "moveToBottomRight", "moveToTopCenter", "moveToBottomCenter");
     outerBoxElement.classList.add(classPosition);
-    console.log('test')
+
 }
 
+const leftButtonElement = document.querySelector(".leftButton");
 leftButtonElement.addEventListener("click", function () {
     movePosition("moveToLeft");
 });
 
 const topLeftButtonElement = document.querySelector(".topLeftButton");
-topLeftButtonElement.addEventListener("click", movePosition("moveToTopLeft"));
+topLeftButtonElement.addEventListener("click", function() {
+    movePosition("moveToTopLeft");
+});
 
 const topRightButtonElement = document.querySelector(".topRightButton");
 topRightButtonElement.addEventListener("click", function() {
-    outerBoxElement.classList.remove("moveToLeft", "moveToTopLeft", "moveToTopRight", "moveToBottomRight", "moveToTopCenter", "moveToBottomCenter");
-    outerBoxElement.classList.add("moveToTopRight");    
+    movePosition("moveToTopRight");
 });
 
 const bottomRightElement = document.querySelector(".bottomRightButton");
 bottomRightElement.addEventListener("click", function() {
-    outerBoxElement.classList.remove("moveToLeft", "moveToTopLeft", "moveToTopRight", "moveToBottomRight", "moveToTopCenter", "moveToBottomCenter");
-    outerBoxElement.classList.add("moveToBottomRight");   
+    movePosition("moveToBottomRight");
 });
 
 const topCenterElement = document.querySelector(".topCenterButton");
 topCenterElement.addEventListener("click", function() {
-    outerBoxElement.classList.remove("moveToLeft");
-    outerBoxElement.classList.remove("moveToTopLeft");
-    outerBoxElement.classList.remove("moveToTopRight");
-    outerBoxElement.classList.remove("moveToBottomRight");
-    outerBoxElement.classList.remove("moveToBottomCenter");
-    outerBoxElement.classList.add("moveToTopCenter");
+    movePosition("moveToTopCenter");
 });
 
 const bottomCenterElement = document.querySelector(".bottomCenterButton");
 bottomCenterElement.addEventListener("click", function() {
-    outerBoxElement.classList.remove("moveToLeft");
-    outerBoxElement.classList.remove("moveToTopLeft");
-    outerBoxElement.classList.remove("moveToTopRight");
-    outerBoxElement.classList.remove("moveToBottomRight");
-    outerBoxElement.classList.remove("moveToTopCenter");
-    outerBoxElement.classList.add("moveToBottomCenter");
+    movePosition("moveToBottomCenter");
 });
-
-
-
