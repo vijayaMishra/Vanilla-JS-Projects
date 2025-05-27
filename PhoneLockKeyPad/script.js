0a335b7d12d8985e0e55d5eaddc1f638b8e858a6
vijayatiwari <vijayamishra1709@gmail.com>
2025-05-27 22:18:04 +0530

    Number display on screen as typing, Align keypad at bottom and display typed numbers at top, mimicking standard keypad layout.

--- PhoneLockKeyPad/script.js
+++ PhoneLockKeyPad/script.js
@@ -1,4 +1,11 @@
-document.querySelector(".oneBtn")
+document.querySelector(".numbersContainer")
     .addEventListener("click", (event)=>{
         console.log(event.target.textContent);
+
+        // console.log(document.querySelector(".displayNumber").innerHTML);
+        let numberOnScreen = document.querySelector(".displayNumber");
+        console.log(numberOnScreen);
+        //= event.target.textContent;
+        //numberOnScreen += event.target.textContent;
+        numberOnScreen.innerHTML += event.target.textContent;
     });
