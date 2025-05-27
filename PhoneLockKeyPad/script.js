3f4e83f2a1ce55b12672d13ee1d6df29c1fb4fdf
vijayatiwari <vijayamishra1709@gmail.com>
2025-05-27 22:30:14 +0530

    Identifying and differentiating between clicked elements like DIV vs BUTTON using event.target.tagName and accessing their content.

--- PhoneLockKeyPad/script.js
+++ PhoneLockKeyPad/script.js
@@ -1,11 +1,13 @@
 document.querySelector(".numbersContainer")
     .addEventListener("click", (event)=>{
         console.log(event.target.textContent);
-
+    console.log(event);
         // console.log(document.querySelector(".displayNumber").innerHTML);
         let numberOnScreen = document.querySelector(".displayNumber");
         console.log(numberOnScreen);
         //= event.target.textContent;
         //numberOnScreen += event.target.textContent;
+        console.log("event.target.tagName",event.target.tagName);
         numberOnScreen.innerHTML += event.target.textContent;
+        console.log("event.target",event.target);
     });
