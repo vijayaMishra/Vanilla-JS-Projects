3399fbd8458af1f2ac47f724c6c1b1555f25c8fa
vijayatiwari <vijayamishra1709@gmail.com>
2025-06-16 16:33:19 +0530

    Hands-on on JS Objects

---
+++ Vanilla JS Programs Practice/Objects/objectCreationAndAccess.js
@@ -0,0 +1,8 @@
+//Create an object person with properties: name, age, and city. Print each property.
+
+const person = {
+    name: "Vijaya Tiwari",
+    age: 100,
+    city: "Bengaluru"
+}
+console.log("Hi My name is ", person["name"], " aged ", person.age, " living in ", person["city"]);
