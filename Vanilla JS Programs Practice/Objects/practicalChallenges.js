8faa54f215c969eae748521274025b027d9b9356
vijayatiwari <vijayamishra1709@gmail.com>
2025-06-17 21:26:09 +0530

    added practical challenges questions

---
+++ Vanilla JS Programs Practice/Objects/practicalChallenges.js
@@ -0,0 +1,4 @@
+// 24. Write a function that takes an object of student marks and returns the average.
+
+
+//25. Write a function that merges two objects into one, resolving conflicts by taking the second object’s values.
