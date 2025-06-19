d11465e3eccceab6b3d564cac9f5a3b819bf3db9
vijayaMishra <vijayamishra1709@gmail.com>
2025-06-19 16:30:16 +0530

    learning spread operator

--- VanillaJSProgramsPractice/Objects/objectCloningAndSpreadOperator.js
+++ VanillaJSProgramsPractice/Objects/objectCloningAndSpreadOperator.js
@@ -1,5 +1,14 @@
+let person = {
+    name: "Vijaya Tiwari",
+    age: 334,
+    city: "Braj"
+}
 // 14. Clone the person object using the spread operator and change the name in the clone.
+//Clone using spread operator
 
+let clonedPerson = { ...person };
+clonedPerson.name = "Gandharvika";
+console.log("Cloned Person", clonedPerson);
 
 // 15. Use Object.assign() to create a shallow copy of an object and modify a property.
 
