4bcc960aa2d5909281debe5e82672eb89d3dfdbb
vijayaMishra <vijayamishra1709@gmail.com>
2025-06-19 18:02:16 +0530

    nested object behaviour with spread operator or Object.assign() i.e., shallow copy duplicates only first level

--- VanillaJSProgramsPractice/Objects/objectCloningAndSpreadOperator.js
+++ VanillaJSProgramsPractice/Objects/objectCloningAndSpreadOperator.js
@@ -11,7 +11,29 @@ clonedPerson.name = "Gandharvika";
 console.log("Cloned Person", clonedPerson);
 
 // 15. Use Object.assign() to create a shallow copy of an object and modify a property.
-
+let copiedPerson2 = Object.assign({}, person);
+copiedPerson2.age = 16;
+console.log(person);
+console.log(copiedPerson2);
 
 // 16. Show how changes in nested properties affect original vs. copied object.
 
+let devotee1 = {
+    name: "Shyam",
+    age: 21,
+    address: {
+        city: "Braj",
+        pin: 281001
+    }
+}
+console.log("Devotee1", devotee1);
+let devotee2 = { ...devotee1 };
+console.log(devotee2);
+
+devotee1.name = "Priyaju";
+console.log(devotee1.name);
+console.log(devotee2.name);
+
+devotee1.address.city = "Barsana";
+console.log(devotee2.address.city);
+console.log(devotee1.address.city);
