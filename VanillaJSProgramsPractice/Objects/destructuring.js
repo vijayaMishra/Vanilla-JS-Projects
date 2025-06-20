945160e8da9abe579d8fe061517b4916034175ef
vijayatiwari <vijayamishra1709@gmail.com>
2025-06-20 23:49:56 +0530

    destructuring arrays

--- VanillaJSProgramsPractice/Objects/destructuring.js
+++ VanillaJSProgramsPractice/Objects/destructuring.js
@@ -1,3 +1,14 @@
+// Destructuring Arrays
+let arr = [10, 20, 30, 40];
+let [a, b, c, d] = arr;
+console.log(a, b, c, d);
+
+let person = {
+    name: "Vijaya Tiwari",
+    city: "Bangalore",
+    spouse: "Ankit Tiwari"
+}
+console.log(person);
 // 17. use destructuring to extract name and age from person.
 
 
