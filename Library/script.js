fd215a69ab258c566ada2c442814298a4cc3f103
vijayaMishra <vijayamishra1709@gmail.com>
2025-07-02 09:22:50 +0530

    adding elements using JS (DOM Manipulation)

---
+++ Library/script.js
@@ -0,0 +1,12 @@
+const books = [
+    {title: "Atomic Habits", author: "James Clear" }, 
+    {title: "Morning Miracle", author: "Hal Elrod" },
+    {title: "Mukund Mala", author: "King Kulshekhara"}
+];
+const ulMyListElement = document.querySelector(".myList");
+
+books.forEach(book => {
+    let liElement = document.createElement("li");
+    liElement.textContent = book.title; 
+    ulMyListElement.appendChild(liElement);
+});
