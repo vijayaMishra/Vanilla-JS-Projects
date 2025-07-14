a92321aa9c1dc0c25cc267c3127e0b074d48d575
vijayatiwari <vijayamishra1709@gmail.com>
2025-07-14 22:45:05 +0530

    introduced a 2D array

--- XOGame/script.js
+++ XOGame/script.js
@@ -1,20 +1,24 @@
 const xoBoxInGridElements = document.querySelectorAll(".xoBoxInGrid");
 let count = 0;
-const movesArray = Array(9).fill(null);
+let board = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
 xoBoxInGridElements.forEach((xoBoxInGridElement, index) => {
     xoBoxInGridElement.addEventListener("click", () => {
-        if(movesArray[index] !== null) {
-            return; // do nothing
-        }
+        // if(movesArray[index] !== null) {
+        //     return; // do nothing
+        // }
         const xoWordElement = document.createElement("p");
         xoWordElement.classList.add("xoWord");
         xoBoxInGridElement.appendChild(xoWordElement);
         //Alternate X and O based on count
         let move = count % 2 ? 'X' : 'O';
         xoWordElement.textContent = move;
-        movesArray[index] = move;
+
         count++;
-        console.log(movesArray);
+        console.log(board);
     });
     
 });
