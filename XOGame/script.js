b9c5b4d8069f60a333d7025467567198fd9f6e91
vijayaMishra <vijayamishra1709@gmail.com>
2025-07-14 09:13:05 +0530

    Taking turns to write X and O, beginning with X

--- XOGame/script.js
+++ XOGame/script.js
@@ -1,10 +1,18 @@
 const xoBoxInGridElements = document.querySelectorAll(".xoBoxInGrid");
+let count = 0;
 xoBoxInGridElements.forEach(function writeXorO(xoBoxInGridElement) {
     xoBoxInGridElement.addEventListener("click", () => {
         const pTagElement = document.createElement("p");
         pTagElement.classList.add("xoWord");
         xoBoxInGridElement.appendChild(pTagElement);
-        pTagElement.textContent = 'X';
-        console.log(xoBoxInGridElement.textContent);
+        count++;
+        if(count % 2 !== 0) { 
+            pTagElement.textContent = 'X';
+            console.log(xoBoxInGridElement.textContent);
+            console.log("no. of clicks", count);
+        } else {
+            pTagElement.textContent = 'O';
+        }
+        
     });
 });
