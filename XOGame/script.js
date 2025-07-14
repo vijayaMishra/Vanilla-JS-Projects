fa7158940a492536d7c861321234e9a288189664
vijayatiwari <vijayamishra1709@gmail.com>
2025-07-14 22:38:49 +0530

    getting all the X and Os stored in an array

--- XOGame/script.js
+++ XOGame/script.js
@@ -3,20 +3,18 @@ let count = 0;
 const movesArray = Array(9).fill(null);
 xoBoxInGridElements.forEach((xoBoxInGridElement, index) => {
     xoBoxInGridElement.addEventListener("click", () => {
-        console.log("index:", index);
-        if(xoBoxInGridElement.querySelector("p")) {
+        if(movesArray[index] !== null) {
             return; // do nothing
         }
         const xoWordElement = document.createElement("p");
         xoWordElement.classList.add("xoWord");
-        xoBoxInGridElement.appendChild(xoWordElement);           
+        xoBoxInGridElement.appendChild(xoWordElement);
         //Alternate X and O based on count
-        if(count % 2 !== 0) { 
-            xoWordElement.textContent = 'X';
-        } else {
-            xoWordElement.textContent = 'O';
-        } 
+        let move = count % 2 ? 'X' : 'O';
+        xoWordElement.textContent = move;
+        movesArray[index] = move;
         count++;
+        console.log(movesArray);
     });
     
 });
