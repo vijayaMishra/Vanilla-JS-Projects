3dd30cfbd8b6fe48af3cf76f7e2cbdb5e7cc9ae3
vijayaMishra <vijayamishra1709@gmail.com>
2025-07-14 10:17:12 +0530

    fixed the issue of multiple X and Os getting added in a box

--- XOGame/script.js
+++ XOGame/script.js
@@ -1,18 +1,20 @@
 const xoBoxInGridElements = document.querySelectorAll(".xoBoxInGrid");
 let count = 0;
 xoBoxInGridElements.forEach(function writeXorO(xoBoxInGridElement) {
-    xoBoxInGridElement.addEventListener("click", () => {
-        const pTagElement = document.createElement("p");
-        pTagElement.classList.add("xoWord");
-        xoBoxInGridElement.appendChild(pTagElement);
-        count++;
-        if(count % 2 !== 0) { 
-            pTagElement.textContent = 'X';
-            console.log(xoBoxInGridElement.textContent);
-            console.log("no. of clicks", count);
-        } else {
-            pTagElement.textContent = 'O';
+    xoBoxInGridElement.addEventListener("click", () => { 
+        if(xoBoxInGridElement.querySelector("p")) {
+            return; // do nothing
         }
-        
+        const xoWordElement = document.createElement("p");
+        xoWordElement.classList.add("xoWord");
+        xoBoxInGridElement.appendChild(xoWordElement);           
+        //Alternate X and O based on count
+        if(count % 2 !== 0) { 
+            xoWordElement.textContent = 'X';
+        } else {
+            xoWordElement.textContent = 'O';
+        } 
+        count++;
     });
+    
 });
