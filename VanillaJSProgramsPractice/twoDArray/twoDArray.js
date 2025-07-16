1408ab2f6598ce5077f43fabbf909a409f73aad6
vijayaMishra <vijayamishra1709@gmail.com>
2025-07-16 10:45:35 +0530

    sum of row elements

--- VanillaJSProgramsPractice/twoDArray/twoDArray.js
+++ VanillaJSProgramsPractice/twoDArray/twoDArray.js
@@ -10,3 +10,22 @@ for(let i = 0; i < 3; i++) {
     }
 }
 console.log("Sum of all elements in the array: ", sum);
+
+//find sum of first row
+let sumFirstRow = 0;
+for(let col = 0; col < 3; col++) {
+    sumFirstRow += numbers[0][col];
+}
+console.log(sumFirstRow);
+
+//WAF to take row as input and return sum of that row
+function calculateRowSum(numbers, row) {
+    let sumOfRow = 0;
+    for(let col = 0; col < 3; col++) {
+        sumOfRow = sumOfRow + numbers[row][col];
+    }
+    return sumOfRow;
+}
+console.log("Sum of first row: ", calculateRowSum(numbers, 0));
+console.log("Sum of second row: ", calculateRowSum(numbers, 1));
+console.log("Sum of third row: ", calculateRowSum(numbers, 2));
