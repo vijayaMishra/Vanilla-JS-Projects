76a2c67315dd15ba9b362fa1dc35ae65e3391103
vijayatiwari <vijayamishra1709@gmail.com>
2025-07-18 22:47:41 +0530

    Printing resolve message after 2 seconds of timer

--- VanillaJSProgramsPractice/Promises/beginnerLevelPromisesProblem.js
+++ VanillaJSProgramsPractice/Promises/beginnerLevelPromisesProblem.js
@@ -1,5 +1,21 @@
 //1. Create a Promise that resolves after 2 seconds with "Hello, World!"
-
+let condition = true;
+let promise = new Promise(function(resolve, reject){
+    setTimeout(function resolveAfter2secs() {
+        if (condition) {
+            resolve("Hello, World!");
+        } else {
+            reject("Not printing anything");
+        }
+    }, 2000);
+});
+promise
+    .then(function(message) {
+        console.log("After 2 seconds: ", message);
+    })
+    .catch(function(error) {
+        console.log("Failure: ", error);
+    });
 
 // 2. Create a function isEvenNumber(num) that returns a Promise
 
