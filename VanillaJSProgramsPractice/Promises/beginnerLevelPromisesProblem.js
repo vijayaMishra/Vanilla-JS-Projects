63aa80c9b477ae56d701ed3369efe90f4e282ce5
vijayatiwari <vijayamishra1709@gmail.com>
2025-07-18 22:40:08 +0530

    added beginner level questions on promises

---
+++ VanillaJSProgramsPractice/Promises/beginnerLevelPromisesProblem.js
@@ -0,0 +1,14 @@
+//1. Create a Promise that resolves after 2 seconds with "Hello, World!"
+
+
+// 2. Create a function isEvenNumber(num) that returns a Promise
+
+
+// 3. Simulate an API call using a Promise
+
+
+// 4. Chaining Promises:
+
+
+
+// 5. Write a Promise that simulates a login check
