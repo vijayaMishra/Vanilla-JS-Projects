d1197216956ce4d4de5051305ab5d42dab0ce54b
vijayatiwari <vijayamishra1709@gmail.com>
2025-07-20 20:24:09 +0530

    promise question 1 to print "Task Completed" upon resolving promise

--- VanillaJSProgramsPractice/Promises/beginnerLevelPromisesProblem.js
+++ VanillaJSProgramsPractice/Promises/beginnerLevelPromisesProblem.js
@@ -4,40 +4,45 @@
 //     console.log("Appears after 1 sec");
 // }, 1000);
 
-
 //create a promise which resolves with value hi after 1 second
-let promise = new Promise(function promiseCallback(resolve){
-    setTimeout(function setTimeoutCallback() {
-        resolve("Hi");
-    }, 1000);
-});
-promise
-    .then(function promiseThenCallback(message){
-        console.log(message);
-    })
+// let promise = new Promise(function promiseCallback(resolve){
+//     setTimeout(function setTimeoutCallback() {
+//         resolve("Hi");
+//     }, 1000);
+// });
+// promise
+//     .then(function promiseThenCallback(message){
+//         console.log(message);
+//     })
 
-
-//1. Create a Promise that resolves after 2 seconds with "Hello, World!"
-// let promise = new Promise(function(resolve, reject){
+// 2. Create a Promise that resolves after 2 seconds with "Hello, World!"
+// let promise2 = new Promise(function(resolve, reject){
 //     setTimeout(function resolveAfter2secs() {
-//         console.log("In setTImout");
+//         console.log("In setTimout");
 //         resolve("Hello, World!");
 //     }, 2000);
 // });
-// console.log(promise);
-// promise
+// console.log(promise2);
+// promise2
 //     .then(function(message) {
 //         console.log("After 2 seconds: ", message);
 //     });
-// console.log("hi");
+// // console.log("hi");
+
+// Create a Promise that resolves after 1 second with the message "Task Completed".
+const promise3 = new Promise(function (resolve) {
+    console.log("After 1 Sec");
+    setTimeout(function setTimeoutCallback() {
+        resolve("Task Completed");
+    }, 1000);
+})
+promise3
+    .then(function (message) {
+        console.log("1 sec is over: ", message);
+    });
+
+
 // 2. Create a function isEvenNumber(num) that returns a Promise
-
-
 // 3. Simulate an API call using a Promise
-
-
 // 4. Chaining Promises:
-
-
-
 // 5. Write a Promise that simulates a login check
