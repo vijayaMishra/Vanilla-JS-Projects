f7e79536b358ff692474049687676ee2b1686838
vijayatiwari <vijayamishra1709@gmail.com>
2025-07-20 18:25:32 +0530

    Make message print after 1000ms

--- VanillaJSProgramsPractice/Promises/beginnerLevelPromisesProblem.js
+++ VanillaJSProgramsPractice/Promises/beginnerLevelPromisesProblem.js
@@ -1,15 +1,26 @@
-//1. Create a Promise that resolves after 2 seconds with "Hello, World!"
-let promise = new Promise(function(resolve, reject){
-    setTimeout(function resolveAfter2secs() {
-        resolve("Hello, World!");
-    }, 2000);
-});
-console.log(promise);
-promise
-    .then(function(message) {
-        console.log("After 2 seconds: ", message);
-    });
+//create a settimeout which console logs after 1 sec
+console.log("Hi");
+setTimeout(function print() {
+    console.log("Appears after 1 sec");
+}, 1000);
 
+
+//create a promise which resolves after 1 secand prints hi
+
+
+//1. Create a Promise that resolves after 2 seconds with "Hello, World!"
+// let promise = new Promise(function(resolve, reject){
+//     setTimeout(function resolveAfter2secs() {
+//         console.log("In setTImout");
+//         resolve("Hello, World!");
+//     }, 2000);
+// });
+// console.log(promise);
+// promise
+//     .then(function(message) {
+//         console.log("After 2 seconds: ", message);
+//     });
+// console.log("hi");
 // 2. Create a function isEvenNumber(num) that returns a Promise
 
 
