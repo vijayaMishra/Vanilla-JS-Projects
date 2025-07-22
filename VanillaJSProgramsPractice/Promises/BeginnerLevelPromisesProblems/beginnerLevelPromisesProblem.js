4fb6d864c441e7a490f1bde32aa71a35f2415b36
vijayaMishra <vijayamishra1709@gmail.com>
2025-07-22 10:18:35 +0530

    simulate fetching of username from db after 2 secs return name on success using promise

--- VanillaJSProgramsPractice/Promises/BeginnerLevelPromisesProblems/beginnerLevelPromisesProblem.js
+++ VanillaJSProgramsPractice/Promises/BeginnerLevelPromisesProblems/beginnerLevelPromisesProblem.js
@@ -64,22 +64,33 @@
 
 //Create a Promise that resolves with a string "Loading finished" only if a variable isLoaded is true. 
 // Otherwise, reject with "Loading failed".
-let isLoaded = true;
-let promise6 = new Promise(function(resolve, reject) {
-    if(isLoaded) {
-        resolve("Loading finished");
-    } else {
-        reject("Loading failed");
-    }
-});
-promise6
-    .then(function(message) {
-        console.log(message);
-    })
-    .catch(function(error){
-        console.log(error);
-    })
+// let isLoaded = true;
+// let promise6 = new Promise(function(resolve, reject) {
+//     if(isLoaded) {
+//         resolve("Loading finished");
+//     } else {
+//         reject("Loading failed");
+//     }
+// });
+// promise6
+//     .then(function(message) {
+//         console.log(message);
+//     })
+//     .catch(function(error){
+//         console.log(error);
+//     })
 
+//Write a Promise that simulates fetching a username from a database after 2 seconds and returns "user123" on success.
+let promise7 = new Promise(function(resolve, reject) {
+    console.log("Timer of 2 secs is on");
+    setTimeout(function fetchName() {
+        resolve("user123");
+    }, 2000);
+});
+promise7
+    .then(function(message) {
+        console.log("After 2 secs:", message);
+    })
 
 // 2. Create a function isEvenNumber(num) that returns a Promise
 // 3. Simulate an API call using a Promise
