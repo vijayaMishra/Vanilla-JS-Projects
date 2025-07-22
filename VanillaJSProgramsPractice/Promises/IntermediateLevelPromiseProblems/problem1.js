d831ab8a4dc08dbf45c91f7926230ea24e301112
vijayatiwari <vijayamishra1709@gmail.com>
2025-07-22 22:17:23 +0530

    problem 4 solving

--- VanillaJSProgramsPractice/Promises/IntermediateLevelPromiseProblems/problem1.js
+++ VanillaJSProgramsPractice/Promises/IntermediateLevelPromiseProblems/problem1.js
@@ -14,9 +14,7 @@ function getLastName() {
 console.log(getFirstName());
 console.log(getFirstName().getLastName);
 console.log(getFirstName().getLastName());
-
 //Create a Promise that resolves to an array ["apple", "banana", "cherry"] and then print each fruit using .then()
-
 
 //Simulate an API call that returns a user's details (object with name and email). Use .then() to log both values.
 
---
+++ VanillaJSProgramsPractice/Promises/IntermediateLevelPromiseProblems/problem4fruits.js
@@ -0,0 +1,10 @@
+//Create a Promise that resolves to an array ["apple", "banana", "cherry"] and then print each fruit
+// using .then()
+
+let promise = new Promise(function(resolve, reject) {
+    resolve(["apple", "banana", "cherry"]);
+})
+promise
+    .then(function printEachFruit(fruit) {
+        console.log(fruit);
+    });
