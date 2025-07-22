1b081f658011e5722ec2b618dab5999fc32fb518
vijayatiwari <vijayamishra1709@gmail.com>
2025-07-22 22:12:36 +0530

    promise that resolves after 2 seconds with user's age. In .then() checking if the age is over 18 and logging adult or minor

--- VanillaJSProgramsPractice/Promises/IntermediateLevelPromiseProblems/problem3checkAdultOrMinor.js
+++ VanillaJSProgramsPractice/Promises/IntermediateLevelPromiseProblems/problem3checkAdultOrMinor.js
@@ -1,7 +1,7 @@
 //Write a Promise that resolves after 2 seconds with a user's age. In .then(), check if the age is over 18 and log "Adult" or "Minor".
 
 let promise = new Promise(function(resolve, reject) {
-    let usersAge = 5;
+    let usersAge = 25;
     setTimeout(function tellAge() {
         resolve(usersAge);
     }, 2000);
@@ -9,4 +9,14 @@ let promise = new Promise(function(resolve, reject) {
 promise
     .then(function(age) {
         console.log("User's age: ", age);
+        return {
+            age: age
+        }
+    })
+    .then(function checkAdultOrMinor(age) {
+        if(age.age >= 18) {
+            console.log(age.age + " is an Adult");
+        } else {
+            console.log(age.age + " is a Minor");
+        }
     })
