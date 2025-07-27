decb65f2dfb3eaababeef5526eeb4d4ae042d544
vijayatiwari <vijayamishra1709@gmail.com>
2025-07-27 22:23:25 +0530

    solved questions given by Ankit sir

--- VanillaJSProgramsPractice/PracticeProblems/practicequestion1.js
+++ VanillaJSProgramsPractice/PracticeProblems/practicequestion1.js
@@ -2,27 +2,34 @@
 // function like getbookfromauthor and that function will accept author
 // for loop laga ke return karo vo book jisme author rhega....
 
-//15:13 
-
 let books = [
     {title: "Morning Miracle", author: "Hal Elrod"},
     {title: "Atomic Habits", author: "James Clear"},
     {title: "The Pscychology of Money", author: "Morgan Housel"},
-    {title: "Mukund Mala"}
+    {title: "Transform Your Habits", author: "James Clear"},
+    {title: "Mukund Mala", author: "King Kulashekhar Alvar"}
 ];
-console.log("First book title: ", books[0].title);
-console.log("First book author: ", books[0].author);
 
-function getBookFromAuthor(author) {
+function getBookFromAuthor(booksArray, author) {
     // not this function should return array of books if same author is found
-
-    // and next create a sep function which will return authos based of book title
-    for(let i=0; i<books.length; i++) {
-        if(books[i].author === author) {
-            return books[i];
+    let booksOfSameAuthor = [];
+    for(let i = 0; i < booksArray.length; i++) {
+        if(booksArray[i].author === author) {
+            booksOfSameAuthor.push(booksArray[i].title);
         }
     }
+    // console.log("booksOfSameAuthor: ", booksOfSameAuthor);
+    return booksOfSameAuthor;
 }
-
-
-console.log("Author name is: ", getBookFromAuthor("Hal Elrod"));
+console.log("Books from the same author are: ", getBookFromAuthor( books, "James Clear"));
+// and next create a sep function which will return author based of book title
+function getAuthorFromBookTitle(books, title) {
+    let authorOfTitle = [];
+    for(let i = 0; i < books.length; i++) {
+        if(books[i].title === title) {
+            authorOfTitle.push(books[i].author);
+        }
+    }
+    return authorOfTitle;
+}
+console.log("Author of the book is: ", getAuthorFromBookTitle(books, "Mukund Mala"));
