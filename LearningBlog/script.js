b0b4c3bec8506959950744fb306a4e173d9049a3
vijayaMishra <vijayamishra1709@gmail.com>
2025-07-30 10:56:20 +0530

    updating learnings of yesterday

--- LearningBlog/script.js
+++ LearningBlog/script.js
@@ -24,6 +24,8 @@ let weeklyStudyData = [
             "learnings": [
                 "Enhancing font styles to improve visual appeal on the Sarjapur Road site.",
                 "If you created a new div element using JS and want to give it 'id' then it can be done using fontsContainerElement.id = 'fontsContainerClass'",
+                "New command: git reset --hard HEAD",
+                "Updating FontsAndColours project, re-doing it through JS instead of adding data in HTML ",
                 "To do: Understand search feature in Fonts page",
                 "To do: Create a custom font-family and add the necessary <link> tag in the Fonts project's homepage to apply it."
             ]
