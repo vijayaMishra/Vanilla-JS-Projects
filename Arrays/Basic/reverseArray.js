2517dc054c56098187bedac6f2158e3dba277a98
vijayaMishra <vijayamishra1709@gmail.com>
2025-08-08 17:51:04 +0530

    reversing an array brute force solution

--- Arrays/Basic/reverseArray.js
+++ Arrays/Basic/reverseArray.js
@@ -5,13 +5,16 @@
 // Output: [4, 3, 2, 1]
 
 let numbers = [1, 2, 3, 4];
+console.log(numbers);
 let reversedArray = [];
 let lastIndex = numbers.length - 1;
 for(let i=0; i < numbers.length; i++) {
-    reversedArray[i] = numbers[lastIndex];
-    let temp = numbers[lastIndex];
-    numbers[i] = temp;
+    reversedArray.push(numbers[lastIndex]);
+    lastIndex--;
+    // let temp = numbers[lastIndex];
+    // numbers[i] = temp;
 }
+console.log(reversedArray);
 
 //dry run space  numbers=[1,2,3,4] reversedArray=[]
 // i=0, i < 4 
