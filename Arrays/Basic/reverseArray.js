bef7b1d8bd8345a2a39daf429287c6326ad96779
vijayaMishra <vijayamishra1709@gmail.com>
2025-08-12 09:08:35 +0530

    reversing an array with inplace swapping, O(1) space complexity

--- Arrays/Basic/reverseArray.js
+++ Arrays/Basic/reverseArray.js
@@ -1,9 +1,9 @@
 // Reverse an Array
 
 // Input: [1, 2, 3, 4]
-
 // Output: [4, 3, 2, 1]
 
+//Brute force approach
 let numbers = [1, 2, 3, 4, 5];
 let reversedArray = [];
 let j = numbers.length - 1;
@@ -15,3 +15,17 @@ console.log(reversedArray);
 
 //dry run space  numbers=[1,2,3,4] reversedArray=[]
 // i=0, i < 4 
+
+//with no extra space
+let array = [1,2,3,4,5];
+console.log("Original Array: ", array);
+let jj = array.length -1;
+for(let i = 0; i < array.length; i++) {
+    if(i < jj) {
+        let temp = array[i];
+        array[i] = array[jj];
+        array[jj] = temp;
+        jj--;
+    }
+}
+console.log("After reverse logic: ", array);
