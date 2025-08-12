7243d33923193d48caa55c690a9da86dec816511
vijayaMishra <vijayamishra1709@gmail.com>
2025-08-12 09:15:18 +0530

    updating Space and Time complexity for the problems solved

--- Arrays/Basic/reverseArray.js
+++ Arrays/Basic/reverseArray.js
@@ -12,6 +12,8 @@ for(let i=0; i<numbers.length; i++) {
     j--;
 }
 console.log(reversedArray);
+//Time Complexity: O(n)
+//Space complexity: O(n) a new array is being created
 
 //dry run space  numbers=[1,2,3,4] reversedArray=[]
 // i=0, i < 4 
@@ -29,7 +31,13 @@ for(let i = 0; i < array.length; i++) {
     }
 }
 console.log("After reverse logic: ", array);
+//Time Complexity O(n)
+//Space Complexity Only jj new variable is created new O(1)
 
 let array1 = [10, 20, 30, 40, 50];
 array1.reverse();
 console.log(array1);
+//Time complexity O(n) 
+// Space Complexity O(1) (in-place)
+// Good for: Quick Reversal (but it mutates the array)
+
