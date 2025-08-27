2b86b9c85698068a73b1c61cf30f056113394248
vijayatiwari <vijayamishra1709@gmail.com>
2025-08-27 22:45:25 +0530

    first react app made

---
+++ FirstReactApp/app.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div style={{ textAlign: "center" }}>
+            Hiiiiii, from VIJAYAAAAAAAA from here 1st React code, Heyyyyyy
+        </div>
+    );
+}
+
+const rootElement = document.querySelector(".root");
+ReactDOM.render(<App />, root); 
