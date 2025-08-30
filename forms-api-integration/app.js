7ffd828d7c774457ebf1a0ad2ddf7bc5e9151ebb
vijayatiwari <vijayamishra1709@gmail.com>
2025-08-30 22:00:51 +0530

    updating useState + handleUsernameChange + handlePasswordChange in app.js

--- forms-api-integration/app.js
+++ forms-api-integration/app.js
@@ -3,21 +3,45 @@
 // custom hooks
 // how to handle loading, error, success states of API
 
-function dummyApi(name, password) {
-    
-}
 
 function App() {
+    //Store what the user types (state)
+    // we will keep username and password in React state and wire the inputs
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    function handleUsernameChange(e) {
+        setUsername(e.target.value);
+    }
+
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+    }
+
+    function handleSubmit() {
+        console.log(username, password);
+    }
+
     return (
-        <div className="body1">
+        <div>
             <h1>Forms and API Integration</h1>
+
             <div className="container">
                 <p style={{color: "black"}}>Username: </p>
-                <input type="text" placeholder="Enter your username"></input>
-                 <p style={{color: "black"}}>Password: </p>
-                <input type="password" placeholder="Enter your password"></input>
+                <input 
+                    type="text" 
+                    placeholder="Enter your username" 
+                    onChange={handleUsernameChange}>
+                </input>
                 
-                <button >Submit</button>
+                <p style={{color: "black"}}>Password: </p>
+                <input 
+                    type="password" 
+                    placeholder="Enter your password" 
+                    onChange={handlePasswordChange} >
+                </input>
+                
+                <button onClick={handleSubmit}>Submit</button>
             </div>
         </div>
     )
